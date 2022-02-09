e4f242ee77444a9c13fcdf7beaa5cdb46785c275
NarguSabz <nargessabbagh@yahoo.com>
2022-02-08 22:56:08 -0500

    Importation de modules necessaires dans serveur.js

---
+++ serveur.js
@@ -0,0 +1,9 @@
+/**
+* import all modules
+**/
+
+var express = require('express');
+var http = require('http');
+var mysql = require('mysql');
+var app = express();
+var bodyParser = require('body-parser');
