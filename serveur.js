b596551caa32ff06e9dce5de02aec6feeb8af66f
NarguSabz <nargessabbagh@yahoo.com>
2022-02-09 20:26:44 -0500

    apportant des changements au serveur.js

--- serveur.js
+++ serveur.js
@@ -26,30 +26,30 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public/'));
 
 //methode http chargee de la route /accueil
-app.get('/accueil',function (req,res) {
+app.get('/accueil', function (req, res) {
     res.render('pages/index.ejs');
 });
 
 //methode http chargee de la route /login
-app.get('/login',function (req,res) {
+app.get('/login', function (req, res) {
     res.render('pages/login.ejs');
 });
 
 //methode http chargee de la route /creerCompte
-app.get('/creerUnCompte',function (req,res) {
+app.get('/creerUnCompte', function (req, res) {
     res.render('pages/creerCompte.ejs');
 });
 
 //methode http chargee de la route /unProduit
-app.get('/unProduit',function (req,res) {
+app.get('/unProduit', function (req, res) {
     res.render('pages/unProduit.ejs');
 });
 
 //methode http chargee de la route /produits
-app.get('/produits',function (req,res) {
+app.get('/produits', function (req, res) {
     res.render('pages/produits.ejs');
 });
 
-var serveur = app.listen(2000, function(){
-	console.log("serveur fonctionne sur 2000... ! ");
+var serveur = app.listen(2000, function () {
+    console.log("serveur fonctionne sur 2000... ! ");
 });
