700f8f394f4edf65935484b7901fc36ed959d043
NarguSabz <nargessabbagh@yahoo.com>
2022-02-20 21:13:11 -0500

    ajout de 2 boucles permettant de remplir chaque pages par des produits

--- serveur.js
+++ serveur.js
@@ -57,6 +57,7 @@ app.get('/produits', function (req, res) {
     //query permettant d aller chercher tous les produits, dans la base de donnees mybd, puis on passe le resultat dans le variable produits
     connection.query("SELECT p.id_produit, p.nom, p.description, p.date_parution, p.prix, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque;",
         function (err, resultat) {
+            //ceci permet de savoir combien de pages sera necessaire pour henberger 20 produits par page
             var nbreDeVingts = parseInt(resultat.length / 20);
             var nbreDePages;
             if (resultat.length % 20 > 0) {
