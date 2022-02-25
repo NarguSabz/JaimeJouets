0bb57abf85fc394633e0ea1304ba08572cbb0491
castiel10k <71992808+castiel10k@users.noreply.github.com>
2022-02-25 15:52:36 -0500

    refractor et cleaning du code avec commentaires

--- serveur.js
+++ serveur.js
@@ -8,7 +8,6 @@ var mysql = require('mysql');
 var app = express();
 var bodyParser = require('body-parser');
 
-
 /*
 * parse all form data
 */
@@ -46,147 +45,129 @@ app.post('/login', function (req, res) {
     var userMessageText = "";
     var userMessageStatus = "";
     //console.log('username used ' + req.body.username);
-    connection.query("Select nom_utilisateur, mdp from compte_client where nom_utilisateur = '" + req.body.username + "'", function(err, result){
+    connection.query("Select nom_utilisateur, mdp from compte_client where nom_utilisateur = '" + req.body.username + "'", function (err, result) {
         if (typeof result[0] == 'undefined') {
-            //console.log('username used ' + req.body.username);
-
+            //message d'erreur pour un nom d'utilsateur incorrecte
             userMessageText = "Combinaison du nom d'utilisateur et mot de passe incorrecte!";
             userMessageStatus = "alertBad";
-
-        }else{
+        } else {
             if (result[0].mdp == req.body.MDP) {
-
+                //message de succes pour une combinaison de nom d'utilisateur et mot de passe correcte
                 userMessageText = "Combinaison du nom d'utilisateur et mot de passe correcte!";
                 userMessageStatus = "alertGood";
-
             } else {
-
+                //message d'erreur pour un mot de passe incorrect
                 userMessageText = "Combinaison du nom d'utilisateur et mot de passe incorrecte!";
                 userMessageStatus = "alertBad";
-                
             }
-            
         }
+        //afficher le message a l'utilisateur
         userMessageArray = [userMessageText, userMessageStatus];
         res.render('pages/login.ejs', { login: "active", accueil: "", creationCompte: "", produit: "", items: userMessageArray });
         res.end();
-
     });
-    
 });
-
-
 
 //methode http chargee de la route /creerCompte
 app.get('/creerUnCompte', function (req, res) {
     //active le lien vers la page de creation du compte et desactive tous les autres liens
     res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "" });
-
 });
 
-
-
-//methode qui se charge d'envoyer les informations necessaires pour la creation d'un compte 
-//vers la BD en s'assurant que ces entrées sont acceptables 
+//methode qui se charge d'envoyer les informations necessaires pour la creation d'un compte
+//vers la BD en s'assurant que ces entrées sont acceptables (select & insert)
 app.post('/creerUnCompte', function (req, res) {
-
     var userMessageText = "";
     var userMessageStatus = "";
-    
-	var resultTest = 0; //initialisation du premier ID a 0 si necessaire
-	
-	if (!checkAllFieldsEmpty(req)){
-		connection.query("SELECT * from panier ORDER BY id_panier DESC LIMIT 1", function (err, result) {
-		
-		if (typeof result[0] != 'undefined') { //chercher le plus gros ID s'il existe pour iterer dessus
-			resultTest = result[0].id_panier;
-			resultTest++;
-		}
-		
-		//verifier si le username existe deja si non, inserer les données de l'utilisateur dans la BD
-		connection.query("SELECT compte_client_nom_utilisateur from panier WHERE compte_client_nom_utilisateur = '" + req.body.username.trim() + "'", function (err, result) {
-			if (typeof result[0] != 'undefined') {
 
-			  userMessageText = "Nom d'utilisateur utilisé";
-                userMessageStatus = "alertBad";
-                userMessageArray = [userMessageText, userMessageStatus];
-                console.log(userMessageArray);
-                res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
-                res.end()
-			} else {
-				connection.query("INSERT INTO panier (id_panier, compte_client_nom_utilisateur) VALUES ( " + resultTest + "," + " '" + req.body.username.trim() + "')", function (err, result) {
-					if (err) {
+    var resultTest = 0; //initialisation du premier ID a 0 si necessaire
 
-                        userMessageText = "Problème lors de l'insertion dans la bd (panier)";
-                        userMessageStatus = "alertBad";
-                        userMessageArray = [userMessageText, userMessageStatus];
-                        console.log(userMessageArray);
-                        res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
-                        res.end()
-                    }
-						connection.query("INSERT INTO compte_client (nom_utilisateur, mdp, prenom, nom ,email, adresse, panier_id_panier) VALUES ( '" + req.body.username + "', '" + req.body.passwordUser + "', '" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.email + "', '" + req.body.adresse + "', " + resultTest + ")", function (err, result) {
-						if (err) {
+    if (!checkAllFieldsEmpty(req)) {
+        connection.query("SELECT * from panier ORDER BY id_panier DESC LIMIT 1", function (err, result) {
+            if (typeof result[0] != 'undefined') { //chercher le plus gros ID s'il existe pour iterer dessus
+                resultTest = result[0].id_panier;
+                resultTest++;
+            }
 
-                            userMessageText = "Problème lors de l'insertion dans la bd (compte_client)";
+            //verifier si le username existe deja si non, inserer les données de l'utilisateur dans la BD
+            connection.query("SELECT compte_client_nom_utilisateur from panier WHERE compte_client_nom_utilisateur = '" + req.body.username.trim() + "'", function (err, result) {
+                if (typeof result[0] != 'undefined') {
+                    //message d'erreur pour un nom d'utilsateur deja pris
+                    userMessageText = "Nom d'utilisateur utilisé";
+                    userMessageStatus = "alertBad";
+                    userMessageArray = [userMessageText, userMessageStatus]; //console.log(userMessageArray);
+                    //afficher le message a l'utilisateur
+                    res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
+                    res.end()
+                } else {
+                    connection.query("INSERT INTO panier (id_panier, compte_client_nom_utilisateur) VALUES ( " + resultTest + "," + " '" + req.body.username.trim() + "')", function (err, result) {
+                        if (err) {
+                            //message d'erreur pour un si il y a une erreur au niveau du sql
+                            //(primary key ou autre) lors de l'insertion dans la table panier
+                            userMessageText = "Problème lors de l'insertion dans la bd (panier)";
                             userMessageStatus = "alertBad";
-                            userMessageArray = [userMessageText, userMessageStatus];
-                            console.log(userMessageArray);
+                            userMessageArray = [userMessageText, userMessageStatus]; //console.log(userMessageArray);
+                            //afficher le message a l'utilisateur
                             res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
                             res.end()
-                        }else{
-                            userMessageText = "Compte Créer avec succès!";
-                            userMessageStatus = "alertGood";
-                            userMessageArray = [userMessageText, userMessageStatus];
-                            console.log(userMessageArray);
-                            res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
-                            res.end()
-                        
                         }
-											
-					});
-				});
-			}
-		});
-	});
+                        connection.query("INSERT INTO compte_client (nom_utilisateur, mdp, prenom, nom ,email, adresse, panier_id_panier) VALUES ( '" + req.body.username + "', '" + req.body.passwordUser + "', '" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.email + "', '" + req.body.adresse + "', " + resultTest + ")", function (err, result) {
+                            if (err) {
+                                //message d'erreur pour un si il y a une erreur au niveau du sql
+                                //(primary key ou autre) lors de l'insertion dans la table compte_client
+                                userMessageText = "Problème lors de l'insertion dans la bd (compte_client)";
+                                userMessageStatus = "alertBad";
+                                userMessageArray = [userMessageText, userMessageStatus]; //console.log(userMessageArray);
+                                //afficher le message a l'utilisateur
+                                res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
+                                res.end()
+                            } else {
+                                //message de succes pour un compte creer
+                                userMessageText = "Compte Créer avec succès!";
+                                userMessageStatus = "alertGood";
+                                userMessageArray = [userMessageText, userMessageStatus]; //console.log(userMessageArray);
+                                //afficher le message a l'utilisateur
+                                res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
+                                res.end()
+                            }
+                        });
+                    });
+                }
+            });
+        });
     } else {
-
+        //message d'erreur pour une entree obligatoire manquante
         userMessageText = "Entrée obligatoire manquante!";
         userMessageStatus = "alertBad";
-        userMessageArray = [userMessageText, userMessageStatus];
-        //console.log(userMessageArray);
+        userMessageArray = [userMessageText, userMessageStatus]; //console.log(userMessageArray);
         res.render('pages/creerUnCompte.ejs', { login: "", accueil: "", creationCompte: "active", produit: "", items: userMessageArray });
         res.end()
     }
-   
 });
 
-function checkAllFieldsEmpty(req){
-
+function checkAllFieldsEmpty(req) {
     var missingAmount = 0;
-    
+
     missingAmount += checkOneFieldEmpty(req.body.username);
     missingAmount += checkOneFieldEmpty(req.body.passwordUser);
     missingAmount += checkOneFieldEmpty(req.body.fname);
     missingAmount += checkOneFieldEmpty(req.body.lname);
     missingAmount += checkOneFieldEmpty(req.body.email);
     missingAmount += checkOneFieldEmpty(req.body.adresse);
-    
-    return Boolean(missingAmount);
 
+    return Boolean(missingAmount);
 }
 
-function checkOneFieldEmpty(fieldToCheck){
-
-    if (fieldToCheck.trim() == ""){
+function checkOneFieldEmpty(fieldToCheck) {
+    if (fieldToCheck.trim() == "") {
         return 1;
     }
     return 0;
 }
 
-
 //methode http chargee de la route /unProduit
 app.get('/unProduit', function (req, res) {
-    res.render('pages/unProduit.ejs', {login: "", accueil: "", creationCompte: "", produit: "active"});
+    res.render('pages/unProduit.ejs', { login: "", accueil: "", creationCompte: "", produit: "active" });
 });
 
 //methode http chargee de la route /produits
@@ -202,7 +183,7 @@ app.get('/produits', function (req, res) {
             } else {
                 nbreDePages = nbreDeVingts;
             }
-            res.render('pages/produits.ejs', {nbrePages: nbreDePages, login: "", accueil: "", creationCompte: "", produit: "active", produits: resultat });
+            res.render('pages/produits.ejs', { nbrePages: nbreDePages, login: "", accueil: "", creationCompte: "", produit: "active", produits: resultat });
         });
     //on active le lien vers la page des produits et desactive tous les autres liens
 });
