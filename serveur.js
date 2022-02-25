7c960179d3972ca0cb7184c50896a0b46effbc62
Sirois-Olivier <78099302+Sirois-Olivier@users.noreply.github.com>
2022-02-25 16:02:22 -0500

    Merge du prototype 1

--- serveur.js
+++ serveur.js
@@ -166,8 +166,19 @@ function checkOneFieldEmpty(fieldToCheck) {
 }
 
 //methode http chargee de la route /unProduit
-app.get('/unProduit', function (req, res) {
-    res.render('pages/unProduit.ejs', { login: "", accueil: "", creationCompte: "", produit: "active" });
+app.get('/produit/:id', function (req, res) {
+    connection.query("SELECT p.id_produit, p.*, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque where p.id_produit =" + req.params.id + ";",
+        function (err, resultat) {
+            if (resultat.length == 0) {
+                console.log("produit non trouvable");
+                res.writeHeader(200, { 'Content-Type': 'text/html ; charset=UTF-8' });
+                res.write("<html><body><script>alert('produit non trouvable'); window.location = 'http://localhost:2000/';</script></body></html>");
+            } else {
+                connection.query("SELECT p.*, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque where p.marques_id_marque ="+resultat[0].marques_id_marque+" and p.id_produit !="+resultat[0].id_produit+";", function (err, result) {
+                    res.render('pages/unProduit.ejs', { login: "", accueil: "", creationCompte: "", produit: "active", produit: resultat[0],produitsDeMemeMarque:result})
+                });
+            };
+        });
 });
 
 //methode http chargee de la route /produits
