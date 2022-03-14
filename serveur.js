7d9fde070a93cb972d8faa29991b96c3decc91ed
castiel10k <71992808+castiel10k@users.noreply.github.com>
2022-03-14 18:30:50 -0400

    testing stills

--- serveur.js
+++ serveur.js
@@ -37,6 +37,7 @@ app.use('/produits', produitsRouter);
 app.use('/produit/:id', produitRouter);
 app.use('/creerUnCompte', creationRouter);
 app.use('/', indexRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -45,12 +46,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error');
 });
 
 var serveur = app.listen(2000, function () {
