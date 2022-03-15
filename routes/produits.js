44e2b46105d1f9718a804e41542f78096ba5e7a5
NarguSabz <nargessabbagh@yahoo.com>
2022-03-15 19:06:49 -0400

    gestion de connection vers la mongodb et ajout de la methode lookup pour chercher le nom de categorie et marque de chacun des produits dans produits.js

--- routes/produits.js
+++ routes/produits.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var http = require('http');
 var mysql = require('mysql');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/protodb');
 var router = express.Router();
 
 //ajout d'une connection a la base de donnees
@@ -8,8 +11,41 @@ var connection = mysql.createConnection({ host: "localhost", user: "root", passw
 
 //methode http chargee de la route /produits
 router.get('/', function (req, res) {
+   var collection = db.get('produits'); 
+   // collection.find({},{},function(e,docs){
+       
+     // res.json(docs[0]._id.numid);
+      //console.log("yess"+JSON.stringify(docs)); });
+      
+      collection.aggregate([
+        { $lookup:
+           {
+             from: 'categories',
+             localField: 'categories_id',
+             foreignField: '_id.numid',
+             as: 'categories_id'
+           } 
+          
+         } ,
+         {$lookup:
+           {
+               from: "marques",
+               localField: "marques_id",
+               foreignField: "_id.numid",
+               as: "marques_id"
+           }}
+        ],function(err, res) {
+            if (err) throw err;
+            console.log(JSON.stringify(res));
+            db.close();
+          });
+    });
+  
+   
+
+    
     //query permettant d aller chercher tous les produits, dans la base de donnees mybd, puis on passe le resultat dans le variable produits
-    connection.query("SELECT p.id_produit, p.nom, p.description, p.date_parution, p.prix, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque;",
+   /* connection.query("SELECT p.id_produit, p.nom, p.description, p.date_parution, p.prix, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque;",
         function (err, resultat) {
             //ceci permet de savoir combien de pages sera necessaire pour henberger 20 produits par page
             var nbreDeVingts = parseInt(resultat.length / 9);
@@ -20,9 +56,8 @@ router.get('/', function (req, res) {
                 nbreDePages = nbreDeVingts;
             }
             res.render('pages/produits.ejs', { nbrePages: nbreDePages, login: "", accueil: "", creationCompte: "", produit: "active", produits: resultat });
-        });
+        });*/
     //on active le lien vers la page des produits et desactive tous les autres liens
-});
 
 
 module.exports = router;
