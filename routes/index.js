5992003260990fbc4de5dad562529722b7cd96f2
NarguSabz <nargessabbagh@yahoo.com>
2022-03-15 19:55:31 -0400

    modification apporter au fichier produits.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/protodb');
 
 var mysql = require('mysql');
 var router = express.Router();
@@ -8,10 +11,37 @@ var connection = mysql.createConnection({ host: "localhost", user: "root", passw
 
 //methode http chargee de la route /accueil
 router.get('/', function (req, res) {
-    //query permettant d aller chercher les 8 les plus recents produits, dans la base de donnees mybd, puis on passe le resultat dans le variable produits
-    connection.query("SELECT p.id_produit, p.nom, p.description, p.date_parution, p.prix, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque order by p.date_parution desc,p.id_produit ASC limit 8;",
+    //ceci permet d aller chercher tous le nom de categorie et de marque de chacun des produits
+    var collection = db.get('produits');
+
+   collection.aggregate([{$orderby: { date_parution : 1 }  },
+     { $lookup:
+        {
+          from: 'categories',
+          localField: 'categories_id',
+          foreignField: '_id.numid',
+          as: 'categories_id'
+        } 
+       
+      } ,
+      {$lookup:
+        {
+            from: "marques",
+            localField: "marques_id",
+            foreignField: "_id.numid",
+            as: "marques_id"
+        }}
+     ],function(err, resultat) {
+         if (err) throw err;           
+
+         console.log(resultat);
+        
+       });
+
+   //query permettant d aller chercher les 8 les plus recents produits, dans la base de donnees mybd, puis on passe le resultat dans le variable produits
+    /*connection.query("SELECT p.id_produit, p.nom, p.description, p.date_parution, p.prix, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque order by p.date_parution desc,p.id_produit ASC limit 8;",
         function (err, resultat) { res.render('pages/index.ejs', { login: "", accueil: "active", creationCompte: "", produit: "", produits: resultat }); });
-    //on active egalement le lien vers la page d accueil et desactive tous les autres liens
+    *///on active egalement le lien vers la page d accueil et desactive tous les autres liens
 });
 
 module.exports = router;
--- routes/produits.js
+++ routes/produits.js
@@ -11,12 +11,8 @@ var connection = mysql.createConnection({ host: "localhost", user: "root", passw
 
 //methode http chargee de la route /produits
 router.get('/', function (req, res) {
-   var collection = db.get('produits'); 
-   // collection.find({},{},function(e,docs){
-       
-     // res.json(docs[0]._id.numid);
-      //console.log("yess"+JSON.stringify(docs)); });
-      
+   var collection = db.get('produits');
+       //ceci permet d aller chercher tous le nom de categorie et de marque de chacun des produits
       collection.aggregate([
         { $lookup:
            {
@@ -37,7 +33,7 @@ router.get('/', function (req, res) {
         ],function(err, resultat) {
             if (err) throw err;           
 
-            console.log(resultat[0].marques_id[0]._id.numid);
+            console.log(resultat);
             //ceci permet de savoir combien de pages sera necessaire pour henberger 20 produits par page
             var nbreDeVingts = parseInt(resultat.length / 9);
             var nbreDePages;
@@ -47,28 +43,11 @@ router.get('/', function (req, res) {
                 nbreDePages = nbreDeVingts;
             }
             res.render('pages/produits.ejs', { nbrePages: nbreDePages, login: "", accueil: "", creationCompte: "", produit: "active", produits: resultat });
-       
+           //on active le lien vers la page des produits et desactive tous les autres liens
+
             db.close();
           });
     });
-  
-   
-
-    
-    //query permettant d aller chercher tous les produits, dans la base de donnees mybd, puis on passe le resultat dans le variable produits
-   /* connection.query("SELECT p.id_produit, p.nom, p.description, p.date_parution, p.prix, c.nom age, m.nom marque from produit p join categories c on p.categories_id_categories = c.id_categories join marques m on p.marques_id_marque = m.id_marque;",
-        function (err, resultat) {
-            //ceci permet de savoir combien de pages sera necessaire pour henberger 20 produits par page
-            var nbreDeVingts = parseInt(resultat.length / 9);
-            var nbreDePages;
-            if (resultat.length % 9 > 0) {
-                nbreDePages = nbreDeVingts + 1;
-            } else {
-                nbreDePages = nbreDeVingts;
-            }
-            res.render('pages/produits.ejs', { nbrePages: nbreDePages, login: "", accueil: "", creationCompte: "", produit: "active", produits: resultat });
-        });*/
-    //on active le lien vers la page des produits et desactive tous les autres liens
 
 
 module.exports = router;
