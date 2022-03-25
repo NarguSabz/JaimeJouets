a38a12a5654f86900be771d38b4a6ec01291d736
NarguSabz <nargessabbagh@yahoo.com>
2022-03-25 14:30:46 -0400

    ajout du route rechercherSuggestions.js pour les requetes ajax de la barre a recherche

---
+++ routes/rechercherSuggestions.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/protodb');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+//methode http chargee de la route /rechercher
+router.get('/', function (req, res) {
+    //ceci permet d aller chercher tous le nom de categorie et de marque de chacun des produits et de aller chercher les 8 les plus recents produits, dans la base de donnees
+   var collection = db.get('produits');
+    
+    collection.aggregate([
+        {
+            $lookup:
+            {
+                from: 'categories',
+                localField: 'categories_id',
+                foreignField: 'numid',
+                as: 'categories_id'
+            }
+
+        },
+        {
+            $lookup:
+            {
+                from: "marques",
+                localField: "marques_id",
+                foreignField: "numid",
+                as: "marques_id"
+            }
+        }, {$match:{ "nom": {$regex: ".*"+ req.query.q +".*" ,$options:"i"}}},{$limit:3}
+    ], function (err, resultat) {
+        
+         res.render('pages/suggestions.ejs', { character:req.query.q,produits: resultat });
+         //on active egalement le lien vers la page d accueil et desactive tous les autres liens        
+        db.close();
+    });
+});
+
+module.exports = router;
