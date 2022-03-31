9c43870f7301a6d7691e0fec66240fb1e74f0d55
castiel10k <71992808+castiel10k@users.noreply.github.com>
2022-03-31 15:30:27 -0400

    captcha ajouter correctement a connexion

--- routes/connexion.js
+++ routes/connexion.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/protodb');
+var request = require('request');
 
 //ajout d'une connection a la base de donnees
 var connection = mysql.createConnection({ host: "localhost", user: "root", password: "", database: "bdproto" });
@@ -22,7 +23,10 @@ router.post('/', function (req, res) {
     if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
         userMessageText = "Captcha non reussis!";
         userMessageStatus = "alertBad";
-    }
+        userMessageArray = [userMessageText, userMessageStatus];
+        res.render('pages/login.ejs', { login: "active", accueil: "", creationCompte: "", produit: "", items: userMessageArray });
+        res.end();
+    } else {
 
     var secretKey = "6LebWCMfAAAAAC70t95BkcQ5JPxaIoFJ-BDxJqg4";
 
@@ -59,7 +63,7 @@ router.post('/', function (req, res) {
 
 
     });
-
+    }
     //console.log('username used ' + req.body.username);
    
     
