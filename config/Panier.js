9642177d71c6c448eb87c40c45eb79ce174d436e
NarguSabz <nargessabbagh@yahoo.com>
2022-04-05 09:37:23 -0400

    ajout du fichier config/panier.js permettant de gerer les differents actions faits sur le panier

---
+++ config/Panier.js
@@ -0,0 +1,62 @@
+
+
+class Panier {
+    constructor() {
+        this.data = {};
+        this.data.produits = [];
+        this.data.totals = 0;
+        this.data.formattedTotals = '';
+     }
+    ajouterAuPanier(produit = null, qty = 1) {
+        if(!this.produitExist(produit.id)) {
+            let prod = {
+                id: produit.id,
+                nom: produit.nom,
+                prix: produit.prix,
+                qty: qty,
+                image: produit.image,
+              };
+            this.data.produits.push(prod);
+            console.log(this.data.produits);
+            this.calculateTotals();
+        }
+    }
+    calculateTotals(){
+        this.data.totals = 0;
+    this.data.produits.forEach(item => {
+        let prix = item.prix;
+        let qty = item.qty;
+        let somme = prix * qty;
+
+        this.data.totals += somme;
+    });  
+    this.setFormattedTotals();
+    }
+    setFormattedTotals() {
+        let format = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'CAD' });
+        let totals = this.data.totals;
+        this.data.formattedTotals = format.format(totals);
+    }
+    enleverProduit(id){
+        for(let i = 0; i < this.data.produits.length; i++) {
+            let item = this.data.produits[i];
+            if(item.id == id) {
+                this.data.produits.splice(i, 1);
+                console.log(this.data.produits);
+                this.calculateTotals();
+            }
+        }
+    }
+
+     produitExist(produitID = 0) {
+        let trouvee = false;
+        this.data.produits.forEach(item => {
+           if(item.id === produitID) {
+            trouvee = true;
+           }
+        });
+        return trouvee;
+    }
+}
+
+module.exports = new Panier();
