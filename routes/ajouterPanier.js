219bd2aec13960cbca9a2f19cedba6a4a5b4ac9a
NarguSabz <nargessabbagh@yahoo.com>
2022-04-05 09:40:25 -0400

    ajout du middleware ajouterPanier.js permettant d effectuer certains taches en se servant de config/panier.js

---
+++ routes/ajouterPanier.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/protodb');
+var router = express.Router();
+const panier = require('../config/Panier.js');
+
+
+//methode http chargee de la route /unProduit
+
+router.post('/', (req, res) => {
+  //si l utilisateur n est pas connectee
+  panier.ajouterAuPanier(req.body.produit,req.body.qty);
+    res.end();
+  });
+  router.get('/', (req, res) => {
+    res.render('../views/pages/panier.ejs', {
+        panier: panier,
+    });
+});
+router.get('/enlever/:id', (req, res) => {
+ let id = req.params.id;
+ panier.enleverProduit(parseInt(id, 10));
+ res.redirect('/ajouterPanier');
+
+  });
+module.exports = router;
