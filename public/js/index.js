07d94d821fef5f5f4ffd95bd79c0e53b8bbed351
NarguSabz <nargessabbagh@yahoo.com>
2022-04-05 22:32:40 -0400

    changements au niveau du panier

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,21 @@
+
+
 function showResult(str, marque) {
 
-	if (str.length == 0) {  
-		document.getElementById("livesearch").innerHTML = "";
-		document.getElementById("livesearch").style.border = "0px";
+    if (str.length == 0) {
+        document.getElementById("livesearch").innerHTML = "";
+        document.getElementById("livesearch").style.border = "0px";
         document.getElementById("layer").style.display = "none";
-		return;
-	}
-    fetch('/rechercherSuggestions?q='+str +"&marque="+marque, {
+        return;
+    }
+    fetch('/rechercherSuggestions?q=' + str + "&marque=" + marque, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-        }}).then(function(response){
-        response.text().then(function(text){
+        }
+    }).then(function (response) {
+        response.text().then(function (text) {
             document.getElementById("livesearch").innerHTML = text;
             document.getElementById("layer").style = "position: fixed; display: block; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0,0,0,0.5); z-index: 99; cursor: pointer;"
             document.getElementsByClassName("livesearch").style = "background-color:white;";
@@ -21,80 +24,95 @@ function showResult(str, marque) {
     })
 
 }
-function ajoutAuPanier(nom,image,id,prix,qty=1) {
-    var produit ={nom:nom,image:image,id:id,prix:prix};
+function ajoutAuPanier(nom, image, id, prix, qty = 1) {
+    var produit = { nom: nom, image: image, id: id, prix: prix };
     const params = {
         produit: produit,
-        qty:qty
+        qty: qty
 
-      }
+    }
     fetch('/panier/ajouterPanier', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            
-        },body:JSON.stringify(params)});
 
-    fetch('/panier/quickview' ,{
+        }, body: JSON.stringify(params)
+    });
+
+    fetch('/panier/quickview', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-        }}).then(function(response){
-            response.text().then(function(text){
+        }
+    }).then(function (response) {
+        response.text().then(function (text) {
 
-                document.getElementById("panier").innerHTML=text;
-    });});}
-    document.getElementById("body").onload = onPageReloud();
-    function onPageReloud(){
-        fetch('/panier/quickview' ,{
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }}).then(function(response){
-                response.text().then(function(text){
-                    
-                        document.getElementById("panier").innerHTML=text;
-                    
-        });});
-    }
-    function enlever(id){
-        fetch('/panier/enlever/'+ id,{
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }}).then(function(response){
-                response.text().then(function(text){
-    
-                        document.getElementById("panier").innerHTML=text;
-                    
-        });});
-    }
-    
-    function enleverPage(id){
-        fetch('/panier/enlever/'+ id,{
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }}).then(function(response){
-                response.text().then(function(text){
-    
-                        document.getElementById("panier").innerHTML=text;
-                        fetch('/panier' ,{
-                            method: 'GET',
-                            headers: {
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json'
-                            }}).then(function(response){
-                                response.text().then(function(text){
-                                    
-                                       document.getElementById("pagePanier").innerHTML=text;
-                                    
-                        });});
+            document.getElementById("panier").innerHTML = text;
+        });
+    });
+}
+document.getElementById("body").onload = onPageReloud();
+function onPageReloud() {
+    fetch('/panier/quickview', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        response.text().then(function (text) {
 
-                    
-        });});
-    }
+            document.getElementById("panier").innerHTML = text;
+
+        });
+    });
+}
+function enlever(id) {
+    fetch('/panier/enlever/' + id, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        response.text().then(function (text) {
+
+            document.getElementById("panier").innerHTML = text;
+
+        });
+    });
+
+}
+
+function enleverPage(id) {
+        fetch('/panier/enlever/' + id, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        response.text().then(function (text1) {
+
+            fetch('/panier', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                response.text().then(function (text) {
+                    document.getElementById("pagePanier").innerHTML = text;
+                    document.getElementById("panier").innerHTML = text1;
+
+
+                });
+            });
+
+
+        });
+    });
+
+
+}
