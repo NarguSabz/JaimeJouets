6dab98a807d85e815866f26d62de163bb6c03d53
NarguSabz <nargessabbagh@yahoo.com>
2022-04-08 16:52:08 -0400

    ajout de la page de checkout

--- routes/commander.js
+++ routes/commander.js
@@ -3,13 +3,15 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/protodb');
 var router = express.Router();
+const panier = require('../config/Panier.js');
+
 
 //methode http chargee de la route /unProduit
 router.get('/', function (req, res) {
   sess = req.session;
   var utilisateur = sess.username;
 
-  res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur });
+  res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur, panier: panier});
   
 });
 
