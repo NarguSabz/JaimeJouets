75bca0cf3b6af86b22576939ade51a2dcd2d472b
castiel10k <71992808+castiel10k@users.noreply.github.com>
2022-04-15 15:53:28 -0400

    refractor complete, started doing edge cases and translations

--- routes/admin.js
+++ routes/admin.js
@@ -17,11 +17,11 @@ router.get('/', function (req, res) {
     sess = req.session;
     utilisateur = sess.username;
 
-    //if (utilisateur == "test") {
+    if (utilisateur == "test") {
         res.render('pages/admin.ejs', { login: "", accueil: "", creationCompte: "", produit: "", username: utilisateur });
-   // } else {
-   //     res.redirect('/');
-   // } 
+    } else {
+        res.redirect('/');
+   } 
 });
 
 router.get('/itemlist', function (req, res) {
@@ -44,38 +44,19 @@ router.post('/', function (req, res) {
         if (typeof result[0] == 'undefined') {  
             userMessageTextGlobal = "l'item " + tempItemId + " est introuvable dans la base de donn�e!";
             userMessageAlertGlobal = "alertBad";
-           
+            printResult(userMessageTextGlobal,userMessageAlertGlobal);
         } else {
+            
             updateDocument("nombrestock", tempReq.body.incSetAmount);
-            updateDocument("prix", tempReq.body.incSetPrice);
+            updateDocument("prix", tempReq.body.incSetPrice)
             positiveDocument();
-        }
+            
+                    
+                }
+  
         
     });
 });
-
-
-function positivePrice(docTmp) {
-
-    db.collection("produits").find({ numid: tempItemId }, function (err, result) {
-
-        if (typeof result[0] == 'undefined') {
-            userMessageTextGlobal = "l'item " + tempItemId + " est introuvable dans la base de donn�e! ";
-            userMessageAlertGlobal = "alertBad";
-
-        } else {
-            if (result[0].prix <= 0) {
-                setPriceZero();
-            } else {
-                userMessageTextGlobal += "nouveau prix de l'item " + tempItemId + " est de : " + result[0].prix + " !";
-                printResult(userMessageTextGlobal, userMessageAlertGlobal);
-            }
-        }
-    });
-
-}
-
-
 
 
 
@@ -83,7 +64,34 @@ function fillVariablesUpdateInput(req) {
     tempItemId = req.body.itemID.toString().trim();
     tempAmountChange = Number(req.body.amountChange);
     tempPriceChange = Number(req.body.priceChange);
+    setEmptyZero();
 }
+
+
+function setEmptyZero() {
+        console.log(tempAmountChange)
+
+
+}
+
+function printFinishedResult(req) {
+    db.collection("produits").find({ numid: tempItemId }, function (err, result) {
+        
+        if (typeof result[0] == 'undefined') {  
+            userMessageTextGlobal = "l'item " + tempItemId + " est introuvable dans la base de donn�e!";
+            userMessageAlertGlobal = "alertBad";
+           
+        } else {
+            stockTmp = setPosInt(result[0].nombrestock);
+            priceTmp = setPosInt(result[0].prix);
+
+            userMessageTextGlobal = "l'item " + tempItemId + " a un stock de "+ stockTmp + " avec le prix de "+ priceTmp +" dollars canadiens!";
+           
+        }
+        printResult(userMessageTextGlobal,userMessageAlertGlobal);
+    });
+}
+
 
 function printResult(userMessageTextTmp, userMessageAlertTmp) {
     userMessageArray = [userMessageTextTmp, userMessageAlertTmp];
@@ -105,56 +113,35 @@ function updateDocument(docTmp, radioToCheck) {
     }
 }
 
-function positiveDocument() {
+function setPosInt(intTmp) {
 
+    if (intTmp <= 0) 
+        return 0;
+    
+    return intTmp;
+}
+
+function positiveDocument(docList) {
     db.collection("produits").find({ numid: tempItemId }, function (err, result) {
         
-        if (typeof result[0] == 'undefined') {
-            userMessageTextGlobal = "l'item " + tempItemId + " est introuvable dans la base de donn�e! ";
-            userMessageAlertGlobal = "alertBad";
+            if (result[0].nombrestock <= 0) 
+                setDocumentZero("nombrestock");
+            
+            if (result[0].prix <= 0) 
+                setDocumentZero("prix");
 
-        } else {
-            if (result[0].nombrestock <= 0) {
-                setQuantityZero("nombrestock");
-            } else {
-                userMessageTextGlobal = "nouveau stock de l'item " + tempItemId + " est de : " + result[0].nombrestock + " ! \n";
-                
-            }
-            if (result[0].prix <= 0) {
-                setPriceZero("prix");
-            } else {
-                userMessageTextGlobal += "nouveau prix de l'item " + tempItemId + " est de : " + result[0].prix + " !";
-                printResult(userMessageTextGlobal, userMessageAlertGlobal);
-            }
-
-        }
     });
-
+    printFinishedResult();    
+   
 }
-function updatePrice(docTmp) {
 
-    if (tempReq.body.incSetPrice == "incPrice") {
-        db.collection("produits").update({ numid: tempItemId }, { $inc: { [docTmp]: tempPriceChange } }).then(() => {
-            positivePrice(docTmp);
+
+function setDocumentZero(docTmp) {
+   
+        db.collection("produits").update({ numid: tempItemId }, { $set: { [docTmp]: 0 } }).then(() => {
+      
         });
-    } else {
-        db.collection("produits").update({ numid: tempItemId }, { $set: { [docTmp]: tempPriceChange } }).then(() => {
-            positivePrice(docTmp);
-        });
-    }
-}
-
-function setQuantityZero(docTmp) {
-    db.collection("produits").update({ numid: tempItemId }, { $set: { [docTmp]: 0 } }).then(() => {
-        userMessageTextGlobal = "nouveau stock de l'item " + tempItemId + " est de : 0 ! "
-    });
-}
-
-function setPriceZero(docTmp) {
-    db.collection("produits").update({ numid: tempItemId }, { $set: { [docTmp]: 0 } }).then(() => {
-        userMessageTextGlobal += "nouveau prix de l'item " + tempItemId + " est de : 0 !";
-        printResult(userMessageTextGlobal, userMessageAlertGlobal);
-    });
+     
 }
 
 
