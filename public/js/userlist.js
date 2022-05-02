a135208006adb84c316444d7e1eeedf8892e68f9
castiel10k <71992808+castiel10k@users.noreply.github.com>
2022-05-01 21:13:21 -0400

    essaies sans succes de faire une table dynamique

--- public/js/userlist.js
+++ public/js/userlist.js
@@ -62,3 +62,33 @@ function showUserInfo(event) {
   $('#userInfoAddresse').text(thisUserObject.adresse);
 });
 };
+
+function deleteUser(event) {
+  event.preventDefault();
+
+  //Pop up a confirmation dialog
+  var confirmation = confirm('Are you sure you want to delete this user?');
+
+  //Check and make sure the user confirmed
+  if (confirmation === true) {
+
+      //If they did, do our delete
+      $.ajax({
+          type: 'DELETE',
+          url: '/admin/deleteuser/' + $(this).attr('rel')
+      }).done(function (response) {
+          //Check for a successful response
+          if (response.msg === '') {
+
+          } else {
+              alert('Error: ' + response.msg);
+          }
+
+          //Update the table
+          populateTable();
+      });
+  } else {
+      //If they said no to the confirm, do notiong
+      return false;
+  }
+};
