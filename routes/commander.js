7e1152954231158a268cb40c430c99f07cb5f745
NarguSabz <nargessabbagh@yahoo.com>
2022-05-03 09:52:24 -0400

    ajout d un redirection de la page de checkout vers la page d accueil quand le panier est vide

--- routes/commander.js
+++ routes/commander.js
@@ -10,9 +10,14 @@ const panier = require('../config/Panier.js');
 router.get('/', function (req, res) {
   sess = req.session;
   var utilisateur = sess.username;
+  if (sess.panier == undefined) {
+    console.log("produit non trouvable");
+    res.writeHeader(200, { 'Content-Type': 'text/html ; charset=UTF-8' });
+    res.write("<html><body><script>alert('votre panier est vide!'); window.location = 'http://localhost:2000/';</script></body></html>");
+  } else {
 
   res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: req.session.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q});
-  
+  }
 });
 
 module.exports = router;
