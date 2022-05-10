f48ee1b6ebfad54c6ce377dff84bf4dd7beb247e
Sirois-Olivier <78099302+Sirois-Olivier@users.noreply.github.com>
2022-05-10 10:09:14 -0400

    Système de rabais terminé

--- routes/commander.js
+++ routes/commander.js
@@ -29,21 +29,53 @@ router.post('/reduction', function (req, res){
     res.write("<html><body><script>alert('votre panier est vide!'); window.location = 'http://localhost:2000/';</script></body></html>");
   } else {
     db.collection("rabais").find({ code: req.body.code }, function (err, result) { 
-      //console.log("1." + result[0].pourcentage + " " + result[0].code);
       if(typeof result[0] == 'undefined'){
         sess.panier.rabais = 0;
+        panier.calculateTotals(sess.panier);
+        userMessageText = "Ce code de réduction n'est pas valide.";
+        userMessageStatus = "alertBad";
+        userMessageArray = [userMessageText, userMessageStatus];
+        if(sess.username){
+          db.collection("panier").update({ compte_client: sess.username }, { $set: { rabais: sess.panier.rabais, formattedSousTotals: sess.panier.formattedSousTotals, formattedTotals: sess.panier.formattedTotals} }).then(() => {
+            res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: sess.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q,items: userMessageArray});
+          });
+        }else{
+          res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: sess.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q,items: userMessageArray});
+        }
       }else if(result[0].utilise == true){
         sess.panier.rabais = 0;
+        panier.calculateTotals(sess.panier);
+        userMessageText = "Ce code de réduction a déjà été utilisé.";
+        userMessageStatus = "alertBad";
+        userMessageArray = [userMessageText, userMessageStatus];
+        if(sess.username){
+          db.collection("panier").update({ compte_client: sess.username }, { $set: { rabais: sess.panier.rabais, formattedSousTotals: sess.panier.formattedSousTotals, formattedTotals: sess.panier.formattedTotals} }).then(() => {
+            res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: sess.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q,items: userMessageArray});
+          });
+        }else{
+          res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: sess.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q,items: userMessageArray});
+        }        
       }else{
-        //console.log("2." + result[0].pourcentage + " " + result[0].code);
         sess.panier.rabais = result[0].pourcentage;
         console.log(sess.panier.rabais);
         panier.calculateRabais(sess.panier, sess.panier.rabais);
         console.log(sess.panier.formattedTotals)
+        userMessageText = "Félicitation! vous avez " + sess.panier.rabais +"% de rabais" ;
+        userMessageStatus = "alertGood";
+        userMessageArray = [userMessageText, userMessageStatus];
+        if(sess.username){
+          db.collection("panier").update({ compte_client: sess.username }, { $set: { rabais: sess.panier.rabais, formattedSousTotals: sess.panier.formattedSousTotals, formattedTotals: sess.panier.formattedTotals} }).then(() => {
+            db.collection("rabais").update({ code: req.body.code }, { $set: { utilise : true} }).then(() => {
+              res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: sess.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q,items: userMessageArray});
+            });
+          });
+        }else{
+          res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: sess.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q,items: userMessageArray});
+        }
+        
       }
     })
 
-    res.render('pages/checkout.ejs', {  login: "", accueil: "", creationCompte: "", produit: "", propos: "", username: utilisateur,classPanier: panier, panier: sess.panier,nbreParPage : 9,recherche:false, marque:req.query.marque,q:req.query.q});
   }
 })
 
